fcd6cee68ef9b7cb6f20e9f9c7d29a4cdaad8c96
SedationH <sedationh@gmail.com>
2026-03-01 19:43:20 +0800

    style: 将安卓主题色改为浅绿色
    - 主色调从紫色改为浅绿色 (#66BB6A / #A5D6A7)
    - 次要色调改为浅绿灰色
    - 第三色调改为青绿色
    - 支持深色和浅色模式

--- VoiceSyncAndroid/app/src/main/java/com/sedationh/voicesync/ui/theme/Color.kt
+++ VoiceSyncAndroid/app/src/main/java/com/sedationh/voicesync/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.sedationh.voicesync.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// 浅绿色主题
+val LightGreen80 = Color(0xFFA5D6A7)  // 浅绿色 - 深色模式主色
+val LightGreenGrey80 = Color(0xFFC5E1A5)  // 浅绿灰 - 深色模式次要色
+val Teal80 = Color(0xFF80CBC4)  // 青绿色 - 深色模式第三色
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightGreen40 = Color(0xFF66BB6A)  // 浅绿色 - 浅色模式主色
+val LightGreenGrey40 = Color(0xFF9CCC65)  // 浅绿灰 - 浅色模式次要色
+val Teal40 = Color(0xFF26A69A)  // 青绿色 - 浅色模式第三色
--- VoiceSyncAndroid/app/src/main/java/com/sedationh/voicesync/ui/theme/Theme.kt
+++ VoiceSyncAndroid/app/src/main/java/com/sedationh/voicesync/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LightGreen80,
+    secondary = LightGreenGrey80,
+    tertiary = Teal80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightGreen40,
+    secondary = LightGreenGrey40,
+    tertiary = Teal40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
